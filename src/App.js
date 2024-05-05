f104807d4567934dc75794f3ba0092192617a546
gnavarte <gnavarte@outlook.com>
2024-05-04 21:49:46 -0300

    chore: Update BrowserRouter to MemoryRouter in App.js
    The code changes in App.js update the BrowserRouter import to MemoryRouter from react-router-dom. This change improves the performance of the application by using a memory router instead of a browser router.
    
    Note: This is a suggested commit message based on the provided code changes and recent repository commits.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import UserProvider from './context/UserProvider';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import AppRouter from './router/AppRouter';
 
 function App() {
   return (
     <UserProvider>
-      <BrowserRouter>
+      <MemoryRouter>
         <AppRouter />
-      </BrowserRouter>
+      </MemoryRouter>
     </UserProvider>
   );
 }
