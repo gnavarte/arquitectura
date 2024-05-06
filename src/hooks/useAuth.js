47c5daf203414c13cec72b518fbbea3103a1350d
gnavarte <gnavarte@outlook.com>
2024-05-06 12:51:03 -0300

    Refactor useAuth to handle user login state

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useContext, useState } from 'react';
 import UserContext from '../context/UserContext';
 import { authLogin, authSignup } from '../services/AuthService';
 
@@ -6,32 +6,27 @@ export const useAuth = () => {
   const { user, setUser } = useContext(UserContext);
   const [errorMessage, setErrorMessage] = useState(undefined);
 
-  const register = async (name, email, password) =>
-    await authSignup(name, email, password).then((data) => {
-      if (data.user) {
-        setUser(data.user);
-      }
-    }).catch((error) => {
-      setErrorMessage(error.response.data.message);
-    });
-
-  const login = async (email, password) => {
-    await authLogin(email, password).then((data) => {
-      if (data.user) {
-        setUser(data.user);
-      }
-    }).catch((error) => {
-      setErrorMessage(error.response.data.message);
-    });
+  const handleResponse = async (response) => {
+    const data = await response.json();
+    if (response.status === 201 || response.status === 200) {
+      setUser(data.user);
+    } else {
+      setErrorMessage(data.message);
+    }
   };
 
-  const logout = async () => {
-    try {
-      // await authService.logout();
-      setUser(null);
-    } catch (error) {
-      setErrorMessage(error.response.data.message);
-    }
+  const register = async (name, email, password) => {
+    const response = await authSignup(name, email, password);
+    await handleResponse(response);
+  };
+
+  const login = async (email, password) => {
+    const response = await authLogin(email, password);
+    await handleResponse(response);
+  };
+
+  const logout = () => {
+    setUser(null);
   };
 
   return {
@@ -41,4 +36,4 @@ export const useAuth = () => {
     logout,
     errorMessage,
   };
-}
+};
