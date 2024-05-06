002eece8258d65edbcc7a56be4843820023e58bf
gnavarte <101013464+gnavarte@users.noreply.github.com>
2024-05-06 17:12:26 -0300

    Refactor AuthService to use environment variables for backend URL and port

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -3,24 +3,21 @@ import { getEnvVariables } from "../helpers";
 const { BACKEND_URL, BACKEND_PORT } = getEnvVariables();
 const authUrl = `${BACKEND_URL}:${BACKEND_PORT}/auth`;
 
-export const authLogin = async (email, password) => {
-    const response = await fetch(`${authUrl}/login`, {
-        method: 'POST',
+const fetchAuthData = async (url, method, data) => {
+    const response = await fetch(url, {
+        method,
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify(data),
     });
     return response;
 };
 
+export const authLogin = async (email, password) => {
+    return fetchAuthData(`${authUrl}/login`, 'POST', { email, password });
+};
+
 export const authSignup = async (name, email, password) => {
-    const response = await fetch(`${authUrl}/signup`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name, email, password }),
-    });
-    return response;
+    return fetchAuthData(`${authUrl}/signup`, 'POST', { name, email, password });
 };
